09e99816aa8b1067337c5d222a079f62a6883593
AJBruyning <aschwinbruyning@outlook.com>
2020-02-12 14:50:47 +0100

    Added Koin implementation to Home module
    + Seperate home viewmodel module
    + Load viewmodel with koin + added comment
    - unused import

--- home/src/main/java/com/example/home/ui/HomeFragment.kt
+++ home/src/main/java/com/example/home/ui/HomeFragment.kt
@@ -7,21 +7,29 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.home.R
+import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.context.loadKoinModules
+
+private val loadModules by lazy { loadKoinModules(viewModelModule) }
+private fun injectFeatures() = loadModules
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel by viewModel<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        //Load the modules
+        injectFeatures()
+
+        //Load fragment
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        //Screen text
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
---
+++ home/src/main/java/com/example/home/ui/HomeModule.kt
@@ -0,0 +1,10 @@
+package com.example.home.ui
+
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+
+val viewModelModule : Module = module {
+    viewModel {HomeViewModel()}
+}
